53b26ca8d4f8b54b9631ec86589b1ff022c8e257
Lars Schütze <lars.schuetze@tu-dresden.de>
2020-03-10 11:17:15 +0100

    Provide simple setup script

---
+++ implementations/setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e # make script fail on first error
+SCRIPT_PATH=`dirname $0`
+source $SCRIPT_PATH/script.inc
+
+echo "Start setup ..."
+# Check for requirements
+check_for_tools ant pip3 python3
+check_for_rebench
+
+echo "\r\nRuntimes:"
+# Install GraalVM
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ -d "$SCRIPT_PATH/graalvm/Contents/Home/" ]]; then
+        echo "INFO: GraalVM ... OK."
+    else
+        echo "ERR: GraalVM ... not found."
+    fi
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    if [[ -d "$SCRIPT_PATH/graalvm/Home/" ]]; then
+        echo "INFO: GraalVM ... OK."
+    else
+        echo "ERR: GraalVM ... not found."
+    fi
+fi
+echo "OK."
+echo "\r\nSetup done."
+
+read -p "Build benchmarks? (y/n)" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "\r\nBuild benchmarks."
+    # Build benchmarks
+    /bin/bash $SCRIPT_PATH/build-java.sh 9
+    /bin/bash $SCRIPT_PATH/build-otj.sh 8 classic
+    /bin/bash $SCRIPT_PATH/build-otj.sh 9 indy
+else
+    echo "Aborting ..."
+fi
+
+
