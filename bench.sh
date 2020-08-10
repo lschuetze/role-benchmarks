573f10d7919412be23704b4a3f3c7917894d5d8c
Lars Schütze <lars.schuetze@tu-dresden.de>
2020-08-10 14:29:42 +0200

    Benchmarks to save results into new location

--- bench.sh
+++ bench.sh
@@ -2,4 +2,19 @@
 
 USER_BASE_PATH=`python3 -m site --user-base`
 
-$USER_BASE_PATH/bin/rebench bench.conf
+$USER_BASE_PATH/bin/rebench -d --scheduler=random --without-nice bench.conf all
+
+DATA_ROOT=./data
+
+REV=`git rev-parse HEAD | cut -c1-8`
+
+NUM_PREV=`ls -l $DATA_ROOT | grep ^d | wc -l`
+NUM_PREV=`printf "%03d" $NUM_PREV`
+
+TARGET_PATH=$DATA_ROOT/$NUM_PREV-$REV
+LATEST=$DATA_ROOT/latest
+
+mkdir -p $TARGET_PATH
+cp benchmark.data $TARGET_PATH/
+rm $LATEST
+ln -s $TARGET_PATH $LATEST
