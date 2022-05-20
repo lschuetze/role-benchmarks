870a948b1f83f0d4702cf47e821fd650b7e694ac
Lars Schütze <lars.schuetze@tu-dresden.de>
2022-05-20 11:18:52 +0200

    Small fix on type signatures

--- benchmarks/jmh-otj/src/main/otj/test/BaseCallTeam.java
+++ benchmarks/jmh-otj/src/main/otj/test/BaseCallTeam.java
@@ -4,8 +4,9 @@ public team class BaseCallTeam {
 
     public class BaseRole playedBy BaseType {
 
-        callin Object noopRole(Object arg) {
-            Object result = base.noopRole(arg);
+        callin int noopRole(int arg) {
+            int result = base.noopRole(arg);
+            result += 1;
             return result;
         }
 
