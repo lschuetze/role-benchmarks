23c2d0b8fad63057be7d0a495c62ab379aff104f
Lars Schütze <lars.schuetze@tu-dresden.de>
2023-09-21 10:22:48 +0200

    Flat role benchmarks for other approaches

---
+++ benchmarks/scroll/src/main/scala/BankBenchmarkFlat.scala
@@ -0,0 +1,50 @@
+import bank._
+
+import scroll.internal.dispatch.DispatchQuery
+
+class BankBenchmarkFlat extends Benchmark {
+
+  var bank_var = new Bank
+
+  override def innerBenchmarkLoop(innerIterations: Int): Boolean = {
+    val bank = bank_var
+
+    for (from <- bank.getCheckingAccounts()) {
+      var amount: Float = from.getBalance() / innerIterations;
+      for (to <- bank.getSavingAccounts()) {
+        (+from).decreate(amount)
+        (+to).increase(amount)
+      }
+    }
+    return true;
+  }
+
+  def setUp(innerIterations: Int): Boolean = {
+    bank_var = new Bank
+    val bank = bank_var
+
+    System.gc()
+
+    for (i <- 1 to innerIterations) {
+      var p = new Person
+      bank.rolePlaying.addPlaysRelation(p, new bank.Customer)
+
+      var sa = new Account(1000.0f)
+      var ca = new Account(1000.0f)
+
+      bank.rolePlaying.addPlaysRelation(sa, new bank.SavingsAccount)
+      bank.rolePlaying.addPlaysRelation(ca, new bank.CheckingsAccount)
+
+      bank.addSavingsAccount(sa)
+      bank.addCheckingsAccount(ca)
+    }
+    return true;
+  }
+
+  override def benchmark(): Object =
+    throw new RuntimeException("Should never be reached");
+
+  override def verifyResult(result: Object): Boolean =
+    throw new RuntimeException("Should never be reached");
+
+}
